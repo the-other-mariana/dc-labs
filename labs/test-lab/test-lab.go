d4d437d4c1d6ded171fe993ae9ab093b57e5fa1d
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-01-29 18:05:28 +0000

    solved test-lab a third time
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	if len(os.Args) < 2 {
+		fmt.Println("Error: No arguments given. Try again :)")
+		return
+	}
+	names := os.Args[1:]
+	name := strings.Join(names, " ")
+	fmt.Printf("Hello %s, Welcome to the jungle\n", name)
 }
