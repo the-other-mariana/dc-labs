8c97df1090fd4856ebd434b89beb118959beb6f7
Mariana Avalos Arce <mariana.avalos.arce@gmail.com>
2021-03-06 02:35:18 +0000

    Client and Server receive all params
    Signed-off-by: Mariana Avalos Arce <mariana.avalos.arce@gmail.com>

--- labs/aws-s3-explorer/awsS3Client.go
+++ labs/aws-s3-explorer/awsS3Client.go
@@ -1,4 +1,36 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"flag"
+)
+
 func main() {
+
+	var proxy = flag.String("proxy", "localhost:8080", "Proxy url.")
+	var bucketName = flag.String("bucket", "none", "S3 bucket name.")
+	var directory = flag.String("directory", "", "Directory name.")
+	flag.Parse()
+	fmt.Printf("Proxy: %v\n", *proxy)
+	fmt.Printf("Bucket Name: %v\n", *bucketName)
+	fmt.Printf("Directory: %v\n", *directory)
+
+	request := fmt.Sprintf("http://%v/example?bucket=%v&dir=%v", *proxy, *bucketName, *directory)
+	resp, err := http.Get(request)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// Print the first 5 lines of the response body.
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan(); i++ {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
 }
--- labs/aws-s3-explorer/awsS3Explorer.go
+++ labs/aws-s3-explorer/awsS3Explorer.go
@@ -1,4 +1,52 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	//"time"
+	"io"
+	"io/ioutil"
+	"log"
+	"strings"
+	//"encoding/xml"
+)
+
+func responseHandler(res http.ResponseWriter, req *http.Request) {
+
+	bucketName := req.FormValue("bucket")
+	url := fmt.Sprintf("https://%v.s3.amazonaws.com", bucketName)
+	resp, err := http.Get(url)
+	if err != nil {
+		panic("Error at S3 connection: " + err.Error())
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic("Error at data reading: " + err.Error())
+	}
+
+	io.WriteString(res, "RESPONSE")
+	io.WriteString(res, "\nURL: " + url)
+    io.WriteString(res, "\nbucket: "+req.FormValue("bucket"))
+    io.WriteString(res, "\ndir: "+req.FormValue("dir"))
+	io.WriteString(res, "\n" + strings.Split(string(data), "\n")[0])
+}
 func main() {
+
+	var port = flag.Int("port", 9000, "Port number.")
+	flag.Parse()
+	fmt.Printf("Service on Port: %v\n", *port) // port is a pointer
+
+	socket := fmt.Sprintf("localhost:%v", *port)
+
+	http.HandleFunc("/example", func(res http.ResponseWriter, req *http.Request) {
+        responseHandler(res, req)
+    })
+	
+	err := http.ListenAndServe(socket, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
